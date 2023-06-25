03a9ba0b37c9f4c4383f738e424406e4c76c726c
Maxim <maksim.shishkin@mail.ru>
2023-06-25 11:45:33 +0500

    Выполнил домашнюю задачу №27

---
+++ ex27/Program.cs
@@ -0,0 +1,17 @@
+﻿//Задача 27
+//Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+Console.Write("Введите число: ");
+int a = int.Parse(Console.ReadLine() ?? "");
+int sum = 0;
+
+while (a > 0)
+{
+    int num = a % 10;
+    a = a / 10;
+    sum = sum + num;
+}
+Console.WriteLine($"сумма всех цифр в числе равна:  {sum}");
+
+
+
