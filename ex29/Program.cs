c27e2b0a81b09f36e4bdc16611dad98492557c30
Maxim <maksim.shishkin@mail.ru>
2023-06-25 11:47:36 +0500

    Выполнил домашнюю задачу №29

---
+++ ex29/Program.cs
@@ -0,0 +1,21 @@
+﻿//Задача№29
+//Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+Console.Write("Введите минимальное число массива: ");
+int min = int.Parse(Console.ReadLine() ?? "");
+Console.Write("Введите максимальное число массива: ");
+int max = int.Parse(Console.ReadLine() ?? "");
+Console.Write("Введите размер массива: ");
+int size = int.Parse(Console.ReadLine() ?? "");
+
+Console.Write("[");
+int count = 1;
+while (count < size)
+{
+    count++;
+    Console.Write(new Random().Next(min, max));
+    Console.Write(", ");
+}
+Console.Write(new Random().Next(min, max));
+Console.WriteLine("]");
+
