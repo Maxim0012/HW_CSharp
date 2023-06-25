cd7263acc7e91b220da4b310a1c50fabccc375d0
Maxim <maksim.shishkin@mail.ru>
2023-06-25 11:43:46 +0500

    Выполнил домашнюю задачу №25

---
+++ ex25/Program.cs
@@ -0,0 +1,17 @@
+﻿
+//Задача 25
+//Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+Console.WriteLine("введите число A");
+int a = int.Parse(Console.ReadLine() ?? "");
+Console.WriteLine("введите число B");
+int b = int.Parse(Console.ReadLine() ?? "");
+int size = a;
+
+for (int i = 1; i < b; i++)
+{
+    size = size * a;
+}
+Console.WriteLine($"{a} в степени {b} = {size}");
+
+
