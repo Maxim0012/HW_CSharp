ea31344934c99366fc3b289b11020b060372a758
Maxim <maksim.shishkin@mail.ru>
2023-06-25 12:50:58 +0500

    Решил домашнюю задачу №34

---
+++ ex34/Program.cs
@@ -0,0 +1,24 @@
+﻿//Задача №34
+//Задайте массив заполненный случайными положительными трёхзначными числами. 
+//Напишите программу, которая покажет количество чётных чисел в массиве.
+Console.Clear();
+Console.Write("Введите размер массива: ");
+int size = int.Parse(Console.ReadLine() ?? "");
+int[] numbers = new int[size];
+FillArrayRandomNumbers(numbers);
+int count = 0;
+
+
+for (int z = 0; z < numbers.Length; z++)
+    if (numbers[z] % 2 == 0)
+        count++;
+
+Console.WriteLine($" {String.Join(" ", numbers)} --> {count} ");
+
+void FillArrayRandomNumbers(int[] numbers)
+{
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = new Random().Next(100, 1000);
+    }
+}
